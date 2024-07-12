afd4b91a3c5b3d7b2d22f5906b6be8e86f4c0622
amine Chatate <amine.chatate@gmail.com>
2024-07-12 20:39:47 +0100

    Add files via upload

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Amine CHATATE on 8/6/2024.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
